13d2cc5c20105ee77c62e26e2bc31e753dd504a5
heliomartinstn-max <heliomartinstn@gmail.com>
2026-05-26 08:15:22 -0300

    Atualização de teste

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/teste")
+def hello_world():
+    return {"mensagem": " Hello World"}
+
+
+@app.get("/soma/{numero1}/{numero2}")
+def soma(numero1: int, numero2: int):
+    total = numero1 + numero2
+    return {"resultado": total}
