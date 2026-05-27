3db776a87c66d5e61dbf067e4a279af08d7ab3a2
heliomartinstn-max <heliomartinstn@gmail.com>
2026-05-27 09:17:41 -0300

    atualização 3

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(app = FastAPI(
+ title="Aula",
+ summary="API desenvolvida durante a aula de Construção de APIs para IA",
+ version="0.1",
+ terms_of_service="http://example.com/terms/",
+ contact={
+ "name": "Hélio Martins T Neto",
+ "url": "http://github.com/rogerior/",
+ "email": "rogerior@ufg.br",
+ },
+ license_info={
+ "name": "Apache 2.0",
+ "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+ },
+))
 
 @app.get("/teste")
 def hello_world():
@@ -26,7 +40,10 @@ class Numeros(BaseModel):
     numero1: int
     numero2: int
  
-@app.post("/soma_formato3")
+class Resultado(BaseModel):
+    resultado: int
+
+@app.post("/soma_formato3", response_model= Resultado)
 def soma_formato3(numeros: Numeros):
     total = numeros.numero1 + numeros.numero2
     return {"resultado": total}
